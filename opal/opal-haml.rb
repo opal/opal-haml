4d1296f5c8c80b1c438d5d49625b4645f18188fa
Adam Beynon <adam.beynon@gmail.com>
2013-12-04 16:29:01 +0000

    Support building dynamic attributes at runtime (Buffer#attributes())

--- opal/opal-haml.rb
+++ opal/opal-haml.rb
@@ -7,5 +7,26 @@ class Template
     end
 
     alias << append
+
+    # allow tags in haml to have dynamic attributes
+    def attributes(class_id, obj_ref, *attributes_hashes)
+      attributes = class_id
+
+      attributes_hashes.each do |hash|
+        attributes.update hash
+      end
+
+      result = attributes.collect do |attr, value|
+        if value == true
+          next " #{attr}"
+        elsif value == false
+          next
+        else
+          " #{attr}='#{value}'"
+        end
+      end
+
+      result.join
+    end
   end
 end
